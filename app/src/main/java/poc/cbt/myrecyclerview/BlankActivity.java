c22ed960278f54dec3ec7c1b25db37245538b616
Thatchai Lertkharom <Thatchai_ler@ascendinter.com>
2017-01-17 11:28:19 +0700

    Remove unused code

--- app/src/main/java/poc/cbt/myrecyclerview/BlankActivity.java
+++ app/src/main/java/poc/cbt/myrecyclerview/BlankActivity.java
@@ -1,31 +1,9 @@
 package poc.cbt.myrecyclerview;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import model.Container;
-import model.Item;
-import utils.Constants;
 
 public class BlankActivity extends AppCompatActivity {
 
--- app/src/main/java/poc/cbt/myrecyclerview/MainActivity.java
+++ app/src/main/java/poc/cbt/myrecyclerview/MainActivity.java
@@ -1,30 +1,17 @@
 package poc.cbt.myrecyclerview;
 
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
